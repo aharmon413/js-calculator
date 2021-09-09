ef97238a694ffe8f26f08562ecc91d126a455415
Amber Harmon <aharmon413@gmail.com>
2021-09-09 16:30:59 -0400

    Added history feature

--- js/app.js
+++ js/app.js
@@ -7,41 +7,45 @@ class Calculator {
         this.num2 = '';
         this.operator = '';
         this.total = '';
+        this.history = '';
         this.onFirstNum = true;
-        //this.history = '';
     }
     bindOnInputReceived(callback){
         this.onInputReceived = callback;
     }
     appendNumber(num) {
+        if (this.operator === '=') {this.init();} // start over if user presses a number without an operator to chain it to the existing equation
+        this.updateHistoryString(num);
         if (this.onFirstNum) {
             this.num1 += num;
-            this.onInputReceived(this.num1);
-        } else if (this.operator === '=') {
-            this.init();
-            this.num1 += num;
-            this.onInputReceived(this.num1);
+            this.onInputReceived(this.num1, this.history);
         } else {
             this.num2 += num;
-            this.onInputReceived(this.num2);
+            this.onInputReceived(this.num2, this.history);
         }
-        //console.log(`num1: ${this.num1} num2: ${this.num2} opr: ${this.operator} total: ${this.total} onFirstNum: ${this.onFirstNum}`);
+        //console.log(`num1: ${this.num1} num2: ${this.num2} opr: ${this.operator} total: ${this.total} onFirstNum: ${this.onFirstNum} history: ${this.history}`); //debug
     }
     setOperator(opr) {
+        if (this.history.substr(-1) === ' ' || this.num1 === '') {return};
+        this.updateHistoryString(opr);
         if (this.onFirstNum && opr != '=') { 
-            if (this.num1 === '') {this.num1 = '0'};
+            if (this.num1 === '') {this.num1 = '0'}; // user didn't input a first number
             this.onFirstNum = false;
-        } else if (this.operator === '=') {
-            this.total = this.calculate(+this.num1, this.operator, +this.num2);
-            this.onInputReceived(this.total);
         } else if (this.num2) {
             this.total = this.calculate(+this.num1, this.operator, +this.num2);
             this.num1 = this.total;
             this.num2 = '';
-            this.onInputReceived(this.total);
         }
+        this.onInputReceived(this.total || this.num1, this.history);
         this.operator = opr;
-        //console.log(`num1: ${this.num1} num2: ${this.num2} opr: ${this.operator} total: ${this.total} onFirstNum: ${this.onFirstNum}`);
+        //console.log(`num1: ${this.num1} num2: ${this.num2} opr: ${this.operator} total: ${this.total} onFirstNum: ${this.onFirstNum} history: ${this.history}`); //debug
+    }
+    updateHistoryString(value) {
+        if (Number.isInteger(+value)) {
+            this.history += value;
+        } else {
+            if (value != '=') {this.history += ` ${value} `;}; // Equals sign doesn't appear in history string
+        }
     }
     calculate(a, opr, b) {
         switch (opr) {
@@ -64,32 +68,31 @@ class Calculator {
 class Display {
     constructor() {
         this.input = $('.input');
-        //this.history = $('.history');
+        this.history = $('.history');
     }
     bindNumberHandler(handler) {
         $('.number').click(function(e) {
-            //console.log('a number was clicked!');
             handler(e.target.innerHTML);
             }
         )
     }
     bindOperatorHandler(handler) {
         $('.operator').click(function(e) {
-            //console.log('an operator was clicked!');
             handler(e.target.innerHTML);
             }
         )
     }
     bindClearHandler(handler) {
         $('.clear').click(() => {
-            //console.log('clear was clicked!');
             this.input.text('0');
+            this.history.text('0');
             handler();
-            }   
+            }
         )
     }
-    updateDisplay(valueToDisplay) {
+    updateDisplay(valueToDisplay, history) {
         this.input.text(valueToDisplay);
+        this.history.text(history);
     }
 }
 
@@ -103,8 +106,8 @@ class Controller {
         this.display.bindOperatorHandler(this.handleOperator);
         this.display.bindClearHandler(this.handleClear);
     }
-    onInputReceived = (valueToDisplay) => {
-        this.display.updateDisplay(valueToDisplay);
+    onInputReceived = (valueToDisplay, history) => {
+        this.display.updateDisplay(valueToDisplay, history);
     }
     handleNumber = num => {
         this.calculator.appendNumber(num);
